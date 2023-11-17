a7f05f7f1930ee5cecb5143154304730c21dced0
Lon Beshiri <lonbeshiri@gmail.com>
2023-11-17 17:39:35 -0500

    fix: dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,4 +4,4 @@ interface IHomePageProps {
 }
 declare function HomePage({}: IHomePageProps): react_jsx_runtime.JSX.Element;
 
-export { HomePage };
+export { HomePage as default };
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,2 +1,2 @@
 import HomePage from './HomePage';
-export { HomePage };
+export default HomePage;
