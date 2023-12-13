5c8e8336ae970b1a3bc53a4effee5da14a8be656
Lon Beshiri <lonbeshiri@gmail.com>
2023-12-13 14:40:49 -0500

    feat: refresh

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,62 +1,35 @@
-'use client'
-
-import { Link } from '@nextui-org/react'
-
 interface IHomePageProps {}
 
 export default function HomePage({}: IHomePageProps) {
   return (
-    <div className="flex self-center max-w-5xl justify-center">
-      <div className="flex flex-col w-2/3 bg-primary p-4 rounded-lg text-xl font-mono text-secondary">
-        <p className="mb-4">
-          <span>tone</span> is an open source, artist &amp; listener owned music
-          listening service.
+    <main className="flex flex-col justify-center items-center bg-[var(--global-lighter)] text-[var(--global-darker)] h-screen">
+      <div className="max-w-md m-4">
+        <p className="font-content text-xl">
+          <span className="font-release font-bold text-3xl">tone</span> is an
+          open source, artist & listener owned music listening service.
         </p>
-        <p className="mb-4">
+        <p className="font-content text-xl my-4">
           We're building it out right now, help us decide what an equitable
-          music marketplace looks like. Come say hello over at our{' '}
-          <Link
-            className="text-xl text-secondary border-b-1 border-b-secondary"
+          music marketplace looks like.
+        </p>
+        <p className="font-content text-xl">
+          Come say hello over at our{' '}
+          <a
             href="https://discord.com/invite/dfky8n7kFv"
-            color="foreground"
-            showAnchorIcon
-            anchorIcon={<i className="fa-fw fa-brands fa-discord pl-1" />}
+            className="font-header underline text-2xl"
           >
-            Discord
-          </Link>{' '}
+            discord
+          </a>{' '}
           or check out our codebase on{' '}
-          <Link
-            className="text-xl text-secondary border-b-1 border-b-secondary"
-            href="https://github.com/sone-dao"
-            color="foreground"
-            showAnchorIcon
-            anchorIcon={<i className="fa-fw fa-brands fa-github pl-1" />}
+          <a
+            href="https://github.com/sone-dao/"
+            className="font-header underline text-2xl"
           >
-            Github
-          </Link>
+            github
+          </a>
           .
         </p>
-        {/*<div>
-          <Input
-            className="mb-4"
-            classNames={{
-              label: 'text-primary',
-              inputWrapper: 'text-primary',
-            }}
-            radius="none"
-            size="lg"
-            type="email"
-            label="E-mail"
-            labelPlacement="inside"
-            variant="flat"
-            placeholder="Enter your e-mail"
-          />
-          <div className="flex gap-2">
-            <Button>Pre-register</Button>
-            <Button>Login</Button>
-          </div>
-          </div>*/}
       </div>
-    </div>
+    </main>
   )
 }
