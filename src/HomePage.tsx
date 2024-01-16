b6fbd8addb09316a9ac4c8d4ecb1b2c846d38842
Lon Beshiri <lonbeshiri@gmail.com>
2024-01-16 12:55:35 -0500

    feat: title

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,35 +1,42 @@
+import Head from 'next/head'
+
 interface IHomePageProps {}
 
 export default function HomePage({}: IHomePageProps) {
   return (
-    <div className="flex items-center justify-center bg-[var(--global-lighter)] text-[var(--global-darker)] h-full">
-      <div className="max-w-md m-4">
-        <p className="font-content text-xl">
-          <span className="font-release font-bold text-4xl">tone</span> is an
-          open source, artist & listener owned music listening service.
-        </p>
-        <p className="font-content text-xl my-4">
-          We're building it out right now, help us decide what an equitable
-          music marketplace looks like.
-        </p>
-        <p className="font-content text-xl">
-          Come say hello over at our{' '}
-          <a
-            href="https://discord.com/invite/dfky8n7kFv"
-            className="font-header underline text-2xl"
-          >
-            discord
-          </a>{' '}
-          or check out our codebase on{' '}
-          <a
-            href="https://github.com/sone-dao/"
-            className="font-header underline text-2xl"
-          >
-            github
-          </a>
-          .
-        </p>
+    <>
+      <Head>
+        <title>Tone - Home</title>
+      </Head>
+      <div className="flex items-center justify-center bg-[var(--global-lighter)] text-[var(--global-darker)] h-full">
+        <div className="max-w-md m-4">
+          <p className="font-content text-xl">
+            <span className="font-release font-bold text-4xl">tone</span> is an
+            open source, artist & listener owned music listening service.
+          </p>
+          <p className="font-content text-xl my-4">
+            We're building it out right now, help us decide what an equitable
+            music marketplace looks like.
+          </p>
+          <p className="font-content text-xl">
+            Come say hello over at our{' '}
+            <a
+              href="https://discord.com/invite/dfky8n7kFv"
+              className="font-header underline text-2xl"
+            >
+              discord
+            </a>{' '}
+            or check out our codebase on{' '}
+            <a
+              href="https://github.com/sone-dao/"
+              className="font-header underline text-2xl"
+            >
+              github
+            </a>
+            .
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
